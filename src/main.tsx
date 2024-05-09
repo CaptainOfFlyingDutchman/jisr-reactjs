67e983b0d108e37ceef9ee474ea3c03567d7ee1a
Manvendra Singh <manvendrask2@gmail.com>
2024-05-09 20:20:39 +0530

    chore: configure jotai devtools

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import { MenuProvider } from "./components/providers/MenuProvider.tsx";
+import { DevTools } from "jotai-devtools";
+import "jotai-devtools/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MenuProvider>
+      {/* DevTools are tree-shakable in Production builds*/}
+      <DevTools />
+
       <App />
     </MenuProvider>
   </React.StrictMode>,
